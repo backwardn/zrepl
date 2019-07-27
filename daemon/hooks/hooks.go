81b72f3ffae520a2545bbc0628d672082eef36c1
Ross Williams <ross@ross-williams.net>
2019-07-27 15:57:16 +0000

    Log hook output
    * Added a subsystem logger for hooks
    * Logging hook commands' stdout and stderr
      separately at levels Info and Warn.
    
    The latter required implementing an io.Writer
    that buffers writes from command.Exec and only
    writes newline-terminated messages to the log.
    The logWriter must be Close()d after command.Wait()
    or command.Run() returns in order to output the last
    line to the log.

--- daemon/hooks/hooks.go
+++ daemon/hooks/hooks.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"time"
 	"os/exec"
+	"sync"
+	"bufio"
+	"bytes"
 
 	"github.com/zrepl/zrepl/logger"
 )
@@ -18,6 +21,10 @@ const (
 
 type Logger = logger.Logger
 
+func WithLogger(ctx context.Context, log Logger) context.Context {
+	return context.WithValue(ctx, contextKeyLog, log)
+}
+
 func getLogger(ctx context.Context) Logger {
 	if log, ok := ctx.Value(contextKeyLog).(Logger); ok {
 		return log
@@ -25,27 +32,83 @@ func getLogger(ctx context.Context) Logger {
 	return logger.NewNullLogger()
 }
 
-func RunHookCommand(ctx context.Context, command string, env map[string]string, timeout time.Duration) error {
-	var log = getLogger(ctx).
-	    WithField("subsystem", "hooks")
+type logWriter struct {
+	mu *sync.Mutex
+	buf bytes.Buffer
+	scanner *bufio.Scanner
+	logFunc func(string)
+	level logger.Level
+}
 
-	// Run hook executable and wait for timeout
-	commandCtx, cancel := context.WithTimeout(ctx, timeout)
+func NewLogWriter(mu *sync.Mutex, logFunc func(string)) *logWriter {
+	w := new(logWriter)
+	w.mu = mu
+	w.scanner = bufio.NewScanner(&w.buf)
+	w.logFunc = logFunc
+	return w
+}
+
+func (w *logWriter) Write(in []byte) (int, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	bufCount, bufErr := w.buf.Write(in)
+	if bufErr != nil {
+		return bufCount, bufErr
+	}
+
+	if w.scanner.Scan() {
+		w.logFunc(w.scanner.Text())
+	}
+
+	return bufCount, nil
+}
+
+func (w *logWriter) Close() error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	w.scanner.Scan()
+	if w.scanner.Err() == nil && w.scanner.Text() != "" {
+		w.logFunc(w.scanner.Text())
+	}
+
+	return nil
+}
+
+
+func RunHookCommand(ctx context.Context, command string, env map[string]string, timeout time.Duration) error {
+	l := getLogger(ctx)
+
+	cmdCtx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
-	commandExec := exec.CommandContext(commandCtx, command)
-	commandEnv := os.Environ()
+	cmdExec := exec.CommandContext(cmdCtx, command)
+	cmdEnv := os.Environ()
 	for k, v := range env {
-		commandEnv = append(commandEnv, fmt.Sprintf("%s=%s", k, v))
+		cmdEnv = append(cmdEnv, fmt.Sprintf("%s=%s", k, v))
 	}
-	commandExec.Env = commandEnv
+	cmdExec.Env = cmdEnv
 
-	output, err := commandExec.CombinedOutput()
+	var scanMutex sync.Mutex
+	logErrWriter := NewLogWriter(&scanMutex, l.Warn)
+	logOutWriter := NewLogWriter(&scanMutex, l.Info)
+	defer logErrWriter.Close()
+	defer logOutWriter.Close()
+
+	cmdExec.Stderr = logErrWriter
+	cmdExec.Stdout = logOutWriter
+	err := cmdExec.Start()
 	if err != nil {
-		log.Error(fmt.Sprintf("%s", output))
+		l.WithError(err).Error("hook command failed to start")
 		return err
 	}
 
-	log.Info(fmt.Sprintf("%s", output))
+	err = cmdExec.Wait()
+	if err != nil {
+		l.WithError(err).Error("hook command exited with error")
+		return err
+	}
+
 	return nil
 }
